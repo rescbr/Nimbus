c014a18efc56d98d4c1df8f40cc04a0f74b09520
Karoline Ikenami <ikenami@users.noreply.github.com>
2013-09-17 17:25:34 -0300

    trending topics

---
+++ Nimbus.Web/Website/Controllers/TrendingTopicsController.cs
@@ -0,0 +1,22 @@
+﻿using Nimbus.Web.API;
+using Nimbus.Web.Security;
+using Nimbus.Web.Website.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using WebApiContrib.Formatting.Html;
+
+namespace Nimbus.Web.Website.Controllers
+{
+    public class TrendingTopicsController : NimbusApiController
+    {
+        public View Get(string redirect = null)
+        {
+            return new View("TrendingTopics", new LoginModel());
+        }
+
+    }
+}
