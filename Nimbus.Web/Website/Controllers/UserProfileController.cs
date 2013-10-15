801fdc6daa7eb509505db0e838ecc53732717918
Ariane Vieira <arianedv@users.noreply.github.com>
2013-10-15 17:19:51 -0300

    ->jcrop com método no controller e atualizando versao do jquery

--- Nimbus.Web/Website/Controllers/UserProfileController.cs
+++ Nimbus.Web/Website/Controllers/UserProfileController.cs
@@ -1,5 +1,6 @@
 ﻿using Nimbus.Web.API;
 using Nimbus.Web.Website.Models;
+using System.Drawing;
 using System.IO;
 using System.Web.Mvc;
 
@@ -11,7 +12,7 @@ namespace Nimbus.Web.Website.Controllers
         [Authorize]
         public ActionResult Index()
         {
-            var channelApi = ClonedContextInstance<API.Controllers.ChannelController>(); 
+            var channelApi = ClonedContextInstance<API.Controllers.ChannelController>();
             var userApi = ClonedContextInstance<API.Controllers.UserController>();
             var msgApi = ClonedContextInstance<API.Controllers.MessageController>();
             var userprofile = new UserProfileModel()
@@ -38,6 +39,42 @@ namespace Nimbus.Web.Website.Controllers
             Request.Files[0].SaveAs(pathFinal);
             return Json(new { url = nomeFinal });
         }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Crop()
+        {
+            var imagem_url = Request.Form["url"];
+            var x1 = int.Parse(Request.Form["x1"]);
+            var x2 = int.Parse(Request.Form["x2"]);
+            var y1 = int.Parse(Request.Form["y1"]);
+            var y2 = int.Parse(Request.Form["y2"]);
+            var nomeFinal = "../uploads/imagem_crop" + Path.GetExtension(imagem_url);
+
+            using (var response = new StreamReader(Server.MapPath(imagem_url)))
+            {
+                Bitmap imagem = new Bitmap(response.BaseStream);
+
+                int largura = x2 - x1;
+                int altura = y2 - y1;
+
+                Bitmap target = new Bitmap(largura, altura);
+                Rectangle cropRect = new Rectangle(x1, y1, largura, altura);
+
+                using (Graphics g = Graphics.FromImage(target))
+                {
+                    g.DrawImage(imagem, new Rectangle(0, 0, largura, altura), cropRect, GraphicsUnit.Pixel);
+                    using (var fileStream = new FileStream(Server.MapPath(nomeFinal), FileMode.OpenOrCreate))
+                    {
+                        target.Save(fileStream, imagem.RawFormat);
+                        fileStream.Flush();
+                    }
+                }
+            }
+            return Json(new { imagem_recortada = nomeFinal });
+        }
+
+
 
     }
 }
