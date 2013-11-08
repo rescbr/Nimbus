3b9789395eacb1ca63155da0fcfec934e005f84a
Renato Schmidt <rescbr@gmail.com>
2013-11-08 18:24:32 -0200

    Commit do sal grosso

--- Nimbus.Web/ExtensionMethods.cs
+++ Nimbus.Web/ExtensionMethods.cs
@@ -20,5 +20,29 @@ namespace Nimbus.Extensions
 
             return val;
         }
+
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            Random rng = new Random();
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
+        public static IList<T> ShuffleCopy<T>(this IEnumerable<T> input)
+        { 
+            List<T> list = new List<T>(input);
+            list.Shuffle();
+            return list;
+        }      
+    
     }
+
+
 }
