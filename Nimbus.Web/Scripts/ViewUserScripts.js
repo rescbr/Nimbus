244b8f1dd4b322f43d00d4d2eec9adce49eadcd1
Ariane Vieira <arianedv@users.noreply.github.com>
2013-11-08 16:23:43 -0200

    ->->funciiiionou =D meu tooltip agora desaparece e aparece mil cliques =D

--- Nimbus.Web/Scripts/ViewUserScripts.js
+++ Nimbus.Web/Scripts/ViewUserScripts.js
@@ -46,72 +46,66 @@ function SendMessageProfile(receiverId)
 function EditProfile() {
 }
 
-function HidePopover(namePopover)
-{
-    $('#'+namePopover).popover('hide');
-}
+ $(function () {
+        $('.bubbleInfo').each(function () {
+            // options
+            var distance = 10;
+            var time = 250;
+            var hideDelay = 500;
 
-$(function () {
-    $('.bubbleInfo').each(function () {
-        // options
-        var distance = 10;
-        var time = 250;
-        var hideDelay = 500;
+            var hideDelayTimer = null;
 
-        var hideDelayTimer = null;
+            // tracker
+            var beingShown = false;
+            var shown = false;
 
-        // tracker
-        var beingShown = false;
-        var shown = false;
+            var trigger = $('.trigger', this);
+            var popup = $('.popup', this).css('opacity', 0);
+            var close = $('.triggerClose', this);
 
-        var trigger = $('.trigger', this);
-        var popup = $('.popup', this).css('opacity', 0);
+            // set the mouseover and mouseout on both element
+            $([trigger.get(0), popup.get(0)]).click(function () {
+                // don't trigger the animation again if we're being shown, or already visible
+                if (beingShown || shown) {
+                    return;
+                } else {
+                    beingShown = true;
 
-        // set the mouseover and mouseout on both element
-        $([trigger.get(0), popup.get(0)]).click(function () {
-         
-            // don't trigger the animation again if we're being shown, or already visible
-            if (beingShown || shown) {
-                return;
-            } else {
-                beingShown = true;
+                    // reset position of popup box
+                    popup.css({
+                        display: 'block' // brings the popup back in to view
+                    })
 
-                // reset position of popup box
-                popup.css({
-                    display: 'block' // brings the popup back in to view
-                })
+                    // (we're using chaining on the popup) now animate it's opacity and position
+                    .animate({
+                        top: '-=' + distance + 'px',
+                        opacity: 1
+                    }, time, 'swing', function () {
+                        // once the animation is complete, set the tracker variables
+                        beingShown = false;
+                        shown = true;
+                    });
+                }
+            });
 
-                // (we're using chaining on the popup) now animate it's opacity and position
-                .animate({
-                    top: '-=' + distance + 'px',
-                    opacity: 1
-                }, time, 'swing', function () {
-                    // once the animation is complete, set the tracker variables
-                    beingShown = false;
-                    shown = true;
-                });
-            }
-        })
-    });
-});
+            $([close.get(0), popup.get(0)]).click(function () {
+                // reset the timer if we get fired again - avoids double animations
+                if (hideDelayTimer) clearTimeout(hideDelayTimer);
 
-/*
-.mouseout(function () {
-            // reset the timer if we get fired again - avoids double animations
-            if (hideDelayTimer) clearTimeout(hideDelayTimer);
-
-            // store the timer so that it can be cleared in the mouseover if required
-            hideDelayTimer = setTimeout(function () {
-                hideDelayTimer = null;
-                popup.animate({
-                    top: '-=' + distance + 'px',
-                    opacity: 0
-                }, time, 'swing', function () {
-                    // once the animate is complete, set the tracker variables
-                    shown = false;
-                    // hide the popup entirely after the effect (opacity alone doesn't do the job)
-                    popup.css('display', 'none');
-                });
-            }, hideDelay);
+                // store the timer so that it can be cleared in the mouseover if required
+                hideDelayTimer = setTimeout(function () {
+                    hideDelayTimer = null;
+                    popup.animate({
+                        top: '-=' + distance + 'px',
+                        opacity: 0
+                    }, '100', 'swing', function () {
+                        // once the animate is complete, set the tracker variables
+                        shown = false;
+                        // hide the popup entirely after the effect (opacity alone doesn't do the job)
+                        popup.css('display', 'none');
+                    });
+                }, '50');
+            });
         });
-*/
+    });   
+
