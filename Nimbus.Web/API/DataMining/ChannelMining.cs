c329785f8cf2728951815c66b293370bb19cadc7
Renato Schmidt <rescbr@gmail.com>
2013-11-14 14:53:30 -0200

    Adiciona ChannelMining

--- Nimbus.Web/API/DataMining/ChannelMining.cs
+++ Nimbus.Web/API/DataMining/ChannelMining.cs
@@ -8,9 +8,28 @@ using System.Collections.Concurrent;
 
 namespace Nimbus.Web.API.DataMining
 {
-    public class ChannelMining:NimbusApiController
+    public class ChannelMining : NimbusApiController
     {
+        void RelatedChannels(int channelId)
+        {
+            using (var db = DatabaseFactory.OpenDbConnection())
+            {
+                var allDifferentChannelsFromFollowers = db.Where<ChannelUser>(chu => chu.ChannelId == channelId)
+                        .Select(s => db.Where<ChannelUser>(t => t.UserId == s.UserId && t.ChannelId != s.ChannelId)
+                        .Select(u => u.ChannelId));
 
-        
+                Dictionary<int, int> relatedCounter = new Dictionary<int, int>();
+                foreach (var neighborChannels in allDifferentChannelsFromFollowers)
+                {
+                    foreach (var neighborChannel in neighborChannels)
+                    {
+                        relatedCounter[neighborChannel] += 1;
+                    }
+                }
+
+
+            }
+        }
+
     }
 }
