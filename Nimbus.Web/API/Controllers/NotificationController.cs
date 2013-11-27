9ddeba8f12989b703668026a6ec43ed90f0a0a30
Renato Schmidt <rescbr@gmail.com>
2013-11-27 12:07:20 -0200

    API para retorno de notificações persistentes

--- Nimbus.Web/API/Controllers/NotificationController.cs
+++ Nimbus.Web/API/Controllers/NotificationController.cs
@@ -1,4 +1,7 @@
-﻿using Nimbus.Web.Notifications;
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Nimbus.Web.Notifications;
+using Nimbus.Web.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,13 +10,44 @@ using System.Web.Http;
 
 namespace Nimbus.Web.API.Controllers
 {
+    ///Abreviações de tipo
+    using MessageNotificationQuery = TableQuery<NotificationTableEntity<Notifications.MessageNotificationModel>>;
+
+
     [NimbusAuthorize]
     public class NotificationController : NimbusApiController
     {
         [HttpGet]
         public List<MessageNotificationModel> GetMessages()
         {
-            return null;
+            DateTime now = DateTime.UtcNow;
+            string nowPartitionKey = NotificationTableEntity.GeneratePartitionKey(NimbusUser.UserId, now);
+            string endOfTimesPK = NotificationTableEntity.GenerateMaxPartitionKey(NimbusUser.UserId);
+            //string nowRowKey = NotificationTableEntity.GenerateRowKey(now);
+
+            CloudStorageAccount _storageAccount = CloudStorageAccount.Parse(NimbusConfig.StorageAccount);
+            CloudTableClient tableClient = _storageAccount.CreateCloudTableClient();
+            CloudTable table = tableClient.GetTableReference(Const.Azure.MessageNotificationsTable);
+
+            //MessageNotificationQuery query = new MessageNotificationQuery().Where(
+            //    TableQuery.CombineFilters(
+            //    TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, userPartitionKey),
+            //    TableOperators.And,
+            //    TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.GreaterThan, nowRowKey)
+            //    )).Take(15);
+
+            MessageNotificationQuery query = new MessageNotificationQuery().Where(
+                TableQuery.CombineFilters(
+                TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.GreaterThanOrEqual, nowPartitionKey),
+                TableOperators.And,
+                TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.LessThan, endOfTimesPK)
+                )).Take(15);
+
+
+            var results = table.ExecuteQuery(query).ToList();
+
+            return results.Select(r => r.DataObject).ToList();
+
         }
     }
 }
--- Nimbus.Web/Utils/NotificationTableEntity.cs
+++ Nimbus.Web/Utils/NotificationTableEntity.cs
@@ -8,8 +8,41 @@ using WindowsAzure.Table.Attributes;
 
 namespace Nimbus.Web.Utils
 {
+    public static class NotificationTableEntity
+    {
+        //http://blog.liamcavanagh.com/2011/11/how-to-sort-azure-table-store-results-chronologically/
+        /// <summary>
+        /// Gera PartitionKey a partir de userid e data
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public static string GeneratePartitionKey(int userId, DateTime date)
+        {
+            //PERIGO: o ano máximo é 9999 e o mês máximo é 99 (para garantir caso aumente o número de meses do ano)
+            return string.Format("{0:D10}:{1:D4}{2:D2}", userId, 9999 - date.Year, 99 - date.Month);
+        }
+
+        public static string GenerateMaxPartitionKey(int userId)
+        {
+            return string.Format("{0:D10}:{1:D4}{2:D2}", userId, 9999, 99);
+        }
+
+
+        /// <summary>
+        /// Gera RowKey a partir de data
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public static string GenerateRowKey(DateTime date)
+        {
+            long howMuchTimeTillTheEnd = long.MaxValue - date.ToFileTimeUtc();
+            return string.Format("{0:D16}", howMuchTimeTillTheEnd / 10000); //em milissegundos
+        }
+    }
     public class NotificationTableEntity<T> : TableEntity
     {
+
         public NotificationTableEntity() { }
 
         public NotificationTableEntity(int userId)
@@ -38,9 +71,8 @@ namespace Nimbus.Web.Utils
 
         internal void SetEntityPartitionRowKey(int userId, DateTime date)
         {
-            long howMuchTimeTillTheEnd = long.MaxValue - date.ToFileTimeUtc();
-            this.PartitionKey = string.Format("{0:D10}:{1:D4}{2:D2}", userId, date.Year, date.Month);
-            this.RowKey = string.Format("{0:D16}", howMuchTimeTillTheEnd / 10000); //em milissegundos
+            this.PartitionKey = NotificationTableEntity.GeneratePartitionKey(userId, date);
+            this.RowKey = NotificationTableEntity.GenerateRowKey(date);
         }
 
         [Ignore]
