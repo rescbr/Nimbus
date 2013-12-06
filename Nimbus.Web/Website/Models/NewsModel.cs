bc07913fe9ed41c8e6f721eb5f98aae5b4aff1de
Ariane Vieira <arianedv@users.noreply.github.com>
2013-12-06 16:44:33 -0200

    ->criando modelos das views

---
+++ Nimbus.Web/Website/Models/NewsModel.cs
@@ -0,0 +1,16 @@
+﻿using Nimbus.Model.Bags;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nimbus.Web.Website.Models
+{
+    public class NewsModel
+    {
+        public List<TopicBag> Topics { get; set; }
+
+        public List<ChannelBag> Channels { get; set; }
+
+    }
+}
--- Nimbus.Web/_Startup/Routing.cs
+++ Nimbus.Web/_Startup/Routing.cs
@@ -50,7 +50,6 @@ namespace Nimbus.Web.Startup
                     action =  "DefaultAction"
                 }
             );
-
             
         }
 
