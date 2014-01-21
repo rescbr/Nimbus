5c39dc5ec9a577857dfd2e49f64d882b5f0b3fca
Ariane Vieira <arianedv@users.noreply.github.com>
2014-01-21 18:05:55 -0200

    ->criando pagina de faq

---
+++ Nimbus.Web/Website/Controllers/NimbusController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nimbus.Web.Website.Controllers
+{
+    [AllowAnonymous]
+    public class NimbusController:NimbusWebController
+    {
+        [HttpGet]
+        public ActionResult Faq()
+        {
+            return View("faq"); 
+        }
+
+        [HttpGet]
+        public ActionResult FaleConosco()
+        {
+            return View("faleconosco");
+        }
+
+        [HttpGet]
+        public ActionResult QuemSomos()
+        {
+            return View("quemsomos");
+        }
+
+        [HttpGet]
+        public ActionResult Vemporai()
+        {
+            return View("vemporai");
+        }
+
+        [HttpGet]
+        public ActionResult Termosdeuso()
+        {
+            return View("termosdeuso");
+        }
+
+
+    }
+}
