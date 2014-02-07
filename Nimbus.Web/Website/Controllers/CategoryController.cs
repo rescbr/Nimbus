923e452e011d2e25143fe63c5e66560765f528ab
Ariane Vieira <arianedv@users.noreply.github.com>
2014-02-07 12:55:28 -0200

    ->criando a pagina de categorias

---
+++ Nimbus.Web/Website/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Nimbus.Model.Bags;
+using Nimbus.Model.ORM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nimbus.Web.Website.Controllers
+{
+    public class CategoryController: NimbusController
+    {
+        public ActionResult Index()
+        {
+            List<ChannelBag> channels = new List<ChannelBag>();
+
+            var categoryApi = ClonedContextInstance<API.Controllers.CategoryController>();
+            var lstCat = new List<Category>();
+            lstCat = categoryApi.showAllCategory();
+
+            ViewBag.lstCat = lstCat.OrderBy(c => c.Name).ToList();
+            return View("Category", channels);
+        }
+    }
+}
---
+++ Nimbus.Web/Website/Models/CategoryModel.cs
@@ -0,0 +1,13 @@
+﻿using Nimbus.Model.Bags;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nimbus.Web.Website.Models
+{
+    public class CategoryModel
+    {
+        public List<ChannelBag> Channels { get; set; }
+    }
+}
